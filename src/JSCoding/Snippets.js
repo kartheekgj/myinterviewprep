f56ffa442df2e170d9ebf614bf1c1af017e765cd
Captain JK <kartheekgj@gmail.com>
2024-06-03 19:41:00 +0530

    Update Snippets.js

--- src/JSCoding/Snippets.js
+++ src/JSCoding/Snippets.js
@@ -67,3 +67,75 @@ function ClosureSum() {
 }
 
 console.log(ClosureSum(10)(2, 3, 4, 5));
+// Reverse a String
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Palindrome Check
+function isPalindrome(str) {
+    return str === str.split('').reverse().join('');
+}
+
+// Factorial Calculation
+function factorial(num) {
+    if (num <= 1) return 1;
+    return num * factorial(num - 1);
+}
+
+// Fibonacci Sequence
+function fibonacci(n) {
+    let fib = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+    return fib;
+}
+
+// Find the Longest Word
+function longestWord(sentence) {
+    const words = sentence.split(' ');
+    let longest = '';
+    for (const word of words) {
+        if (word.length > longest.length) {
+            longest = word;
+        }
+    }
+    return longest;
+}
+
+// Anagram Check
+function isAnagram(str1, str2) {
+    return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+
+// Sum of Array Elements
+function sumArray(arr) {
+    return arr.reduce((total, num) => total + num, 0);
+}
+
+// Remove Duplicates from Array
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
+// Flatten Nested Array
+function flattenArray(arr) {
+    return arr.reduce((flat, item) => flat.concat(Array.isArray(item) ? flattenArray(item) : item), []);
+}
+
+// Merge Two Sorted Arrays
+function mergeSortedArrays(arr1, arr2) {
+    const merged = [];
+    let i = 0, j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            merged.push(arr1[i++]);
+        } else {
+            merged.push(arr2[j++]);
+        }
+    }
+    return [...merged, ...arr1.slice(i), ...arr2.slice(j)];
+}
+
+
